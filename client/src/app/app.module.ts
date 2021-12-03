7618f02adf71e906df1697102b3aeeb20260261d
SebasttianM <sebasttianvmarin@gmail.com>
2021-12-02 20:35:41 -0500

    Article

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,9 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HeaderComponent } from './components/header/header.component';
+import { ArticuloComponent } from './components/articulo/articulo.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ArticuloComponent
   ],
   imports: [
     BrowserModule,
